42ff3b6cceadb558c81a4443f7f412597e8faf0a
iagows <el.iagows@gmail.com>
2018-05-29 19:02:02 -0300

    Improved GUI and usability

--- app/src/main/java/com/iaspp/prepareyourself/views/ShowMovieInfo.java
+++ app/src/main/java/com/iaspp/prepareyourself/views/ShowMovieInfo.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,12 +30,14 @@ public class ShowMovieInfo extends AppCompatActivity {
         final TextView genre;
         final TextView release;
         final TextView overview;
+        final Button share;
 
         image = findViewById(R.id.image_stub);
         title = findViewById(R.id.title_upcoming);
         genre = findViewById(R.id.genres_upcoming);
         release = findViewById(R.id.release_upcoming);
         overview = findViewById(R.id.tf_overview);
+        share = findViewById(R.id.bt_share);
 
         title.setText(dto.getOriginalTitle());
         genre.setText(dto.getGenreWithComma());
@@ -42,5 +45,11 @@ public class ShowMovieInfo extends AppCompatActivity {
         overview.setText(dto.getOverview());
 
         Picasso.get().load(dto.getFullUrl()).into(image);
+
+
+    }
+
+    private void onShare(){
+
     }
 }
